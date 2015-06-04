dafd5e8b9a4d97acf9881778737fd2730834a101
Samy Pessé <samypesse@gmail.com>
2015-06-04 18:18:47 +0200

    Fix fs.readdir for localfs and add tests for it

--- test/local.js
+++ test/local.js
@@ -1,5 +1,6 @@
 var Q = require('q');
 var path = require('path');
+var _ = require('lodash');
 
 var repofs = require('../');
 var DriverLocal = require('../lib/drivers/local');
@@ -27,6 +28,27 @@ describe('Local Driver', function() {
 
         it('should fail for file out of the repo', function() {
             return fs.read('../../local.js').should.be.rejected;
+        });
+    });
+
+    describe('fs.readdir', function() {
+        it('should correctly read the root directory', function() {
+            return fs.readdir()
+            .then(function(files) {
+                files.should.have.property('README.md');
+
+                files['README.md'].type.should.equal('file');
+                files['README.md'].name.should.equal('README.md');
+                files['README.md'].path.should.equal('README.md');
+            });
+        });
+
+        it('should correctly read a sub-directory', function() {
+            return fs.readdir('./lib')
+            .then(function(files) {
+                files.should.not.have.property('README.md');
+                (_.size(files) > 0).should.equal(true);
+            });
         });
     });
 });
