f36b33a4b288b9f3b8e6e7c052f3cb6ed5c22867
Samy Pessé <samypesse@gmail.com>
2015-06-04 16:20:45 +0200

    Add base structure for api

--- test/local.js
+++ test/local.js
@@ -1,14 +1,26 @@
+var Q = require('q');
+var path = require('path');
+
 var repofs = require('../');
 var DriverLocal = require('../lib/drivers/local');
 
 describe('Local Driver', function() {
     var fs = repofs(DriverLocal, {
-
+        root: path.resolve(__dirname, './fixtures')
     });
 
     describe('fs.read', function() {
         it('should correctly read from master', function() {
+            return fs.read('README.md');
+        });
 
+        it('should fail for file out of the repo', function() {
+            return fs.read('../../local.js')
+            .then(function() {
+                throw "Should have failed";
+            }, function() {
+                return Q();
+            });
         });
     });
 });
