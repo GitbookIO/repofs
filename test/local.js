b1164d78fe2a41cfcc75b5bcd6a0ed76a251152a
Samy Pessé <samypesse@gmail.com>
2015-06-04 16:37:55 +0200

    Complete basic tests

--- test/local.js
+++ test/local.js
@@ -1,5 +1,10 @@
 var Q = require('q');
 var path = require('path');
+var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+chai.should();
 
 var repofs = require('../');
 var DriverLocal = require('../lib/drivers/local');
@@ -11,16 +16,11 @@ describe('Local Driver', function() {
 
     describe('fs.read', function() {
         it('should correctly read from master', function() {
-            return fs.read('README.md');
+            return fs.read('README.md').should.eventually.equal('# Hello')
         });
 
         it('should fail for file out of the repo', function() {
-            return fs.read('../../local.js')
-            .then(function() {
-                throw "Should have failed";
-            }, function() {
-                return Q();
-            });
+            return fs.read('../../local.js').should.be.rejected;
         });
     });
 });
