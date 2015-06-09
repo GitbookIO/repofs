9b59705af63d45bf5c28020bab85a7289ba55d64
Samy Pessé <samypesse@gmail.com>
2015-06-09 15:04:41 +0200

    Add option on getCommit to parse patches

--- test/patch.js
+++ test/patch.js
@@ -10,5 +10,11 @@ describe('Patch', function() {
         patch.compare('A', 'AB').should.equal('@@ -1 +1,2 @@\n A\n+B\n');
     });
 
+    it('should correctly parse a patch', function() {
+        var r = patch.parse(patch.compare('A', 'AB'));
+        r.additions.should.equal(1);
+        r.deletions.should.equal(0);
+    });
+
 });
 
