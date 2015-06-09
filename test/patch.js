79ca7020a00ca0aaf47cd62d25731f3fb65d8a69
Samy Pessé <samypesse@gmail.com>
2015-06-09 15:18:34 +0200

    Use line level diffs

--- test/patch.js
+++ test/patch.js
@@ -7,13 +7,13 @@ var patch = require('../lib/utils/patch');
 describe('Patch', function() {
 
     it('should have correct generate a patch', function() {
-        patch.compare('A', 'AB').should.equal('@@ -1 +1,2 @@\n A\n+B\n');
+        patch.compare('A', 'AB').should.equal('@@ -1 +1,2 @@\n-A\n+AB\n');
     });
 
     it('should correctly parse a patch', function() {
         var r = patch.parse(patch.compare('A', 'AB'));
         r.additions.should.equal(1);
-        r.deletions.should.equal(0);
+        r.deletions.should.equal(1);
     });
 
 });
