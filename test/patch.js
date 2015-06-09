4cdbf9bec0fe7af5183e5b94e550d3a5e64c7f5c
Samy Pessé <samypesse@gmail.com>
2015-06-09 16:11:27 +0200

    Fix diff multilines

--- test/patch.js
+++ test/patch.js
@@ -10,6 +10,10 @@ describe('Patch', function() {
         patch.compare('A', 'AB').should.equal('@@ -1 +1,2 @@\n-A\n+AB\n');
     });
 
+    it('should have correct generate a patch (multiple lines)', function() {
+        patch.compare('A\nB\nC', 'A\nB\nD\nC').should.equal('@@ -1,5 +1,7 @@\n A%0AB%0A\n+D%0A\n C\n');
+    });
+
     it('should correctly parse a patch', function() {
         var r = patch.parse(patch.compare('A', 'AB'));
         r.additions.should.equal(1);
