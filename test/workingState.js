854cb3ac25b1bddba53dfe9f9906d9f6aa0fae62
Samy Pesse <samypesse@gmail.com>
2016-03-16 09:52:34 +0100

    Base for testing

--- test/workingState.js
+++ test/workingState.js
@@ -1,21 +1,32 @@
 require('should');
-var WorkingState = require('../lib/models/workingState');
+
+var repofs = require('../');
+
 
 describe('WorkingState', function() {
 
     describe('.isClean', function() {
 
         it('should true if workingState has not changes', function() {
-            var workingState = WorkingState.createEmpty();
+            var workingState = repofs.WorkingState.createEmpty();
             workingState.isClean().should.equal(true);
         });
 
         it('should false if workingState has changes', function() {
-            var workingState = WorkingState.createEmpty();
-
-
-
-            workingState.isClean().should.equal(true);
+            var workingState = repofs.decodeWorkingState({
+                tree: {
+                    'README.md': {
+                        size: 10,
+                        sha: '...'
+                    }
+                },
+                changes: {
+                    'README.md': {
+                        type: repofs.CHANGE.UPDATE
+                    }
+                }
+            });
+            workingState.isClean().should.equal(false);
         });
 
     });
