9e8e51dd2fe2d75c2de666b66f393d4f4a5ffe8f
Soreine <soreine.plume@gmail.com>
2016-03-17 16:34:14 +0100

    Adds test mocks

---
+++ test/mock.js
@@ -0,0 +1,57 @@
+
+var immutable = require('immutable');
+
+var Cache = require('../lib/models/cache');
+var CacheUtils = require('../lib/utils/cache');
+var Change = require('../lib/models/change');
+var TreeEntry = require('../lib/models/treeEntry');
+var Branch = require('../lib/models/branch');
+var WorkingState = require('../lib/models/workingState');
+var RepositoryState = require('../lib/models/repositoryState');
+
+// Creates a clean repoState for a default book with branches and files already fetched
+// * SUMMARY.md "# Summary"
+// * README.md "# Introduction"
+function defaultBook() {
+    var readmeEntry = new TreeEntry({
+        blobSize: 14,
+        sha: 'readmeSha',
+        mode: '100644'
+    });
+
+    var summaryEntry = new TreeEntry({
+        blobSize: 9,
+        sha: 'summarySha',
+        mode: '100644'
+    });
+
+    var masterBranch = new Branch({
+        shortName: 'master',
+        sha: 'masterSha',
+        remote: ''
+    });
+
+    var workingState = new WorkingState({
+        head: "defaultBookWorkingSha",
+        treeEntries: new immutable.Map({
+            "README.md": readmeEntry,
+            "SUMMARY.md": summaryEntry
+        })
+        // No changes
+    });
+
+    // Already fetched blobs
+    var cache = new Cache();
+    cache = CacheUtils.addBlob(cache, {sha: 'readmeSha', content:'# Introduction'});
+    cache = CacheUtils.addBlob(cache, {sha: 'summarySha', content:'# Summary'});
+
+    return new RepositoryState({
+        currentBranchName: 'master',
+        workingStates: new immutable.Map().set(masterBranch.getName(), workingState),
+        branches: new immutable.Map().set(masterBranch.getName(), masterBranch)
+    });
+}
+
+module.exports = {
+    defaultBook: defaultBook
+};
--- test/workingState.js
+++ test/workingState.js
@@ -1,33 +1,21 @@
 require('should');
 
 var repofs = require('../');
-
+var mock = require('./mock');
 
 describe('WorkingState', function() {
 
     describe('.isClean', function() {
 
         it('should true if workingState has not changes', function() {
-            var workingState = repofs.WorkingState.createEmpty();
-            workingState.isClean().should.equal(true);
+            var repoState = mock.defaultBook();
+            repoState.getCurrentState().isClean().should.equal(true);
         });
 
         it('should false if workingState has changes', function() {
-            var workingState = repofs.decodeWorkingState({
-                tree: {
-                    'README.md': {
-                        blobSize: 10,
-                        sha: '...',
-                        mode: '100644'
-                    }
-                },
-                changes: {
-                    'README.md': {
-                        type: repofs.CHANGE.UPDATE
-                    }
-                }
-            });
-            workingState.isClean().should.equal(false);
+            var repoState = mock.defaultBook();
+            repofs.FileUtils.write(repoState, 'README.md', 'New content');
+            repoState.getCurrentState().isClean().should.equal(false);
         });
 
     });
