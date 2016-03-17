34d5952d08a9a35a85407d9ddf489b51464ad3be
Soreine <soreine.plume@gmail.com>
2016-03-17 13:37:59 +0100

    Writing decoding tests

--- test/decoding.js
+++ test/decoding.js
@@ -1,25 +1,48 @@
 require('should');
+var immutable = require('immutable');
+var _ = require('lodash');
 
 var repofs = require('../');
-var decodeChange = require('../lib/decoding/change');
-var decodeTreeEntry = require('../lib/decoding/treeEntry');
+
+var Change = require('../lib/models/change');
+var TreeEntry = require('../lib/models/treeEntry');
+var Branch = require('../lib/models/branch');
+
 
 describe('Decoding', function() {
 
-    var encodedChange = {
-        type: repofs.CHANGE.UPDATE
-    };
-
-    var encodedTreeEntry = {
-        size: 10,
-        sha: '...',
-        mode: '100644'
-    };
-
-    it('should decode a change', function() {
-        var change = decodeChange(encodedChange);
-        change.getType().should.equal(repofs.CHANGE.UPDATE);
+    var change = new Change({
+        type: repofs.CHANGE.UPDATE,
+        content: new Buffer('ChangeBuffer'),
+        sha:'changeSha',
+        message: 'changeMessage'
     });
+
+    var branch = new Branch({
+        shortName: 'branchShortName',
+        sha: 'branchSha',
+        remote: 'branchRemote'
+    });
+
+    var treeEntry = new TreeEntry({
+        size: 10,
+        sha: 'treeEntrySha',
+        mode: '100644'
+    });
+
+    function testDecodeEncode(type, source) {
+        var encdec = _.flow(type.encode, type.decode);
+        var degcenc = _.flow(type.decode, type.encode);
+
+        immutable.is(source, encdec(source)).should.be.true();
+
+        var encoded = type.encode(source);
+        encoded.should.eql(decenc(encoded));
+    }
+
+    it('should encode and decode back a Change', testDecodeEncode(Change, change));
+    it('should encode and decode back a TreeEntry', testDecodeEncode(TreeEntry, treeEntry));
+    it('should encode and decode back a Branch', testDecodeEncode(Branch, branch));
 
     it('should decode a treeEntry', function() {
         var treeEntry = decodeTreeEntry(encodedTreeEntry);
@@ -27,6 +50,7 @@ describe('Decoding', function() {
     });
 
     it('should decode existing empty working state', function() {
+        var emptyWorking = repofs.decodeWorkingState({ });
     });
 
     it('should decode existing working state', function() {
