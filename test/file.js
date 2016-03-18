ed48624d4b611906827bfd4939727d1585e53e3a
Soreine <soreine.plume@gmail.com>
2016-03-18 16:22:16 +0100

    Finish tests for file. Just need them to pass

--- test/file.js
+++ test/file.js
@@ -1,6 +1,7 @@
 var should = require('should');
 
 var repofs = require('../');
+var bufferUtils = require('../lib/utils/arraybuffer');
 var FileUtils = repofs.FileUtils;
 var mock = require('./mock');
 
@@ -25,7 +26,73 @@ describe('FileUtils', function() {
         it('should read content as ArrayBuffer if file exists', function() {
             var read = FileUtils.read(defaultBook, 'README.md');
             read.should.be.an.instanceof(ArrayBuffer);
-            read.byteLength.should.equal(14);
+            bufferUtils.enforceString(read).should.equal('# Introduction');
+        });
+
+        it('should read content as ArrayBuffer for modified files', function() {
+            var modifiedState = FileUtils.write(defaultBook, 'README.md', 'New');
+            var read = FileUtils.read(defaultBook, 'README.md');
+            read.should.be.an.instanceof(ArrayBuffer);
+            bufferUtils.enforceString(read).should.equal('New');
+        });
+
+        it('should throw File Not Found when file does not exist', function() {
+            (function readUnknown() {
+                FileUtils.read(defaultBook, 'Notexist.md');
+            }).should.throw(Error, { code: repofs.ERROR.NOT_FOUND });
+        });
+    });
+
+    describe('.readAsString', function() {
+        it('should read content as String if file exists', function() {
+            var read = FileUtils.readAsString(defaultBook, 'SUMMARY.md');
+            read.should.be.equal('# Summary');
+        });
+
+        it('should throw File Not Found when file does not exist', function() {
+            (function readUnknown() {
+                FileUtils.readAsString(defaultBook, 'Notexist.md');
+            }).should.throw(Error, { code: repofs.ERROR.NOT_FOUND });
+        });
+    });
+
+    describe('.create', function() {
+        it('should create a file if it does not exist', function() {
+            var repoState = FileUtils.create(defaultBook, 'New', 'New Content');
+            FileUtils.exists(repoState, 'New').should.be.true();
+            FileUtils.readAsString(repoState, 'New').should.be.equal('New Content');
+        });
+
+        it('should throw File Already Exists when file does exist', function() {
+            (function createExisting() {
+                var read = FileUtils.create(defaultBook, 'README.md', '');
+            }).should.throw(Error, { code: repofs.ERROR.ALREADY_EXIST });
+        });
+    });
+
+    describe('.write', function() {
+        it('should write a file if it exists', function() {
+            var repoState = FileUtils.write(defaultBook, 'README.md', 'New Content');
+            FileUtils.readAsString(repoState, 'README.md').should.be.equal('New Content');
+        });
+
+        it('should throw File Not Found when file does not exist', function() {
+            (function writeAbsent() {
+                var read = FileUtils.write(defaultBook, 'Notexist.md', '');
+            }).should.throw(Error, { code: repofs.ERROR.NOT_FOUND });
+        });
+    });
+
+    describe('.remove', function() {
+        it('should remove a file if it exists', function() {
+            var repoState = FileUtils.remove(defaultBook, 'README.md');
+            FileUtils.exists(repoState, 'README.md').should.equal(false);
+        });
+
+        it('should throw File Not Found when file does not exist', function() {
+            (function removeAbsent() {
+                var read = FileUtils.remove(defaultBook, 'Notexist.md');
+            }).should.throw(Error, { code: repofs.ERROR.NOT_FOUND });
         });
     });
 });
