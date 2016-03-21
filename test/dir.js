25a3182b578631b0aadeb6e48cb7fa51c6b858dc
Soreine <soreine.plume@gmail.com>
2016-03-21 11:08:53 +0100

    Implementing read shallow dir

--- test/dir.js
+++ test/dir.js
@@ -11,10 +11,10 @@ describe('DirUtils', function() {
 
     var INITIAL_FILES = [
         'file.root',
-        'dir.twoItems/file1',
-        'dir.twoItems/file2',
-        'dir.deep.oneItem/file1',
-        'dir.deep.oneItem/dir.oneItem/file1'
+        'dir/file1',
+        'dir/file2',
+        'dir.deep/file1',
+        'dir.deep/dir/file1'
     ];
 
     var NESTED_DIRECTORY = mock.directoryStructure(INITIAL_FILES);
@@ -26,9 +26,52 @@ describe('DirUtils', function() {
             _.difference(INITIAL_FILES, files).should.be.empty();
         });
 
+        it('should list files recursively from dir', function() {
+            var filesDeep = DirUtils.readRecursive(NESTED_DIRECTORY, 'dir/');
+            _.difference([
+                'dir/file1',
+                'dir/file2',
+            ], filesDeep).should.be.empty();
+        });
+
+        it('should be flexible with paths', function() {
+            var pathsToTest = [
+                'dir.deep',
+                'dir.deep/',
+                './dir.deep/',
+            ].map(function(path) {
+                return DirUtils.readRecursive(NESTED_DIRECTORY, path);
+            }).map(function (files) {
+                _.difference([
+                    'dir.deep/file1',
+                    'dir.deep/dir/file1'
+                ], files).should.be.empty();
+            });
+        });
     });
 
     describe('.read', function() {
+        it('should shallow list root files', function() {
+            var files = DirUtils.read(NESTED_DIRECTORY, './');
+            _.difference(['file.root'], files).should.be.empty();
+        });
+
+        it('should shallow list all files in a dir', function() {
+            var files = DirUtils.read(NESTED_DIRECTORY, 'dir');
+            _.difference([
+                'dir/file1',
+                'dir/file2',
+            ], files).should.be.empty();
+        });
+
+        it('should shallow list all files and dir in a dir', function() {
+            var files = DirUtils.read(NESTED_DIRECTORY, './dir.deep/');
+            console.log(files);
+            _.difference([
+                'dir.deep/file1',
+                'dir.deep/dir',
+            ], files).should.be.empty();
+        });
     });
 
     describe('.move', function() {
