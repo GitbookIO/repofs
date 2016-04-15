a89f72f48acf381919cc5a544010f739d9e06b85
Soreine <nicolas@gitbook.com>
2016-04-15 15:47:59 +0200

    Implement CommitUtils.listCommit and test (not passing)

--- test/api/commit.js
+++ test/api/commit.js
@@ -6,26 +6,50 @@ module.exports = function (driver) {
 
 // Test the commit API on a basic repo
 function testCommit(driver) {
+
+    var repoState;
+
+    before(function () {
+        return repofs.RepoUtils.initialize(driver)
+        .then(function (initRepo) {
+            repoState = initRepo;
+        });
+    });
+
     describe('.prepare -> .flush', function() {
         it('should flush a prepared commit', function () {
-            repofs.RepoUtils.initialize(driver)
-            .then(function (repoState) {
-                // Create a file for test
-                repoState = repofs.FileUtils.create(
-                    repoState, 'flushCommitFile', 'flushCommitContent');
-                var commitBuilder = repofs.CommitUtils.prepare(repoState, {
-                    author: 'Shakespeare',
-                    message: 'Test message'
-                });
+            // Create a file for test
+            repoState = repofs.FileUtils.create(
+                repoState, 'flushCommitFile', 'flushCommitContent');
+            var commitBuilder = repofs.CommitUtils.prepare(repoState, {
+                author: 'Shakespeare',
+                message: 'Test message'
+            });
 
-                repofs.CommitUtils.flush(commitBuilder)
-                .then(function (repoState) {
-                    // No more pending changes
-                    repoState.getCurrentState().isClean().should.be.true();
+            return repofs.CommitUtils.flush(repoState, driver, commitBuilder)
+            .then(function (newState) {
+                repoState = newState;
+                // No more pending changes
+                repoState.getCurrentState().isClean().should.be.true();
 
-                    // The file was created
-                    repofs.FileUtils.exists(repoState, 'flushCommitFile').should.be.true();
-                });
+                // The file was created
+                repofs.FileUtils.exists(repoState, 'flushCommitFile').should.be.true();
+            });
+        });
+    });
+
+    describe('.listCommits', function() {
+        // Depends on previous test
+        it('should list commits on current branch', function () {
+            repofs.CommitUtils.listCommits(repoState, driver)
+            .then(function (commits) {
+                commits.count().should.be.greaterThan(1);
+                var commit = commits.first();
+                commit.getAuthor().getName().should.eql('Shakespeare');
+                commit.getMessage().should.eql('Test message');
+                commit.getMessage().should.eql('Test message');
+                commit.getFiles().count().should.eql(1);
+                commit.getFiles().first().getPath().should.eql('flushCommitFile');
             });
         });
     });
