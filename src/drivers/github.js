740683681596768dd8521f1206202a4ac9a602a0
Mickael Daniel <daniel.mickael@gmail.com>
2016-12-15 16:11:30 +0100

    Update uhub to 3.1.0

--- src/drivers/github.js
+++ src/drivers/github.js
@@ -254,17 +254,17 @@ class GitHubDriver extends Driver {
 
     // ---- Only supported by uhub ----
     checkout(branch) {
-        return this.post('checkout', {
+        return this.post('local/checkout', {
             branch: branch ? branch.getFullName() : 'HEAD'
         });
     }
 
     listRemotes() {
-        return this.get('remotes');
+        return this.get('local/remotes');
     }
 
     editRemotes(name, url) {
-        return this.post('remotes', {
+        return this.post('local/remotes', {
             name,
             url
         });
@@ -277,7 +277,7 @@ class GitHubDriver extends Driver {
         // Convert to ref
         opts.branch = opts.branch.getName();
 
-        return this.post('pull', opts)
+        return this.post('local/pull', opts)
         .fail(normNotFF)
         .fail(normAuth)
         .fail(normUnknownRemote)
@@ -291,14 +291,14 @@ class GitHubDriver extends Driver {
         // Convert to ref
         opts.branch = opts.branch.getName();
 
-        return this.post('push', opts)
+        return this.post('local/push', opts)
         .fail(normNotFF)
         .fail(normAuth)
         .fail(normUnknownRemote);
     }
 
     status(opts) {
-        return this.get('status')
+        return this.get('local/status')
             .then(function(status) {
                 return {
                     files: new List(status.files).map(function(file) {
@@ -340,7 +340,7 @@ class GitHubDriver extends Driver {
             };
         }
 
-        return this.post('track', params);
+        return this.post('local/track', params);
     }
 
     // API utilities
